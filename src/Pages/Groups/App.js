585d98b0f476ef475fdb4f5142ebdd245d5634e7
Bayasgalan <cetrion8@gmail.con>
2023-11-17 17:29:31 -0800

    Linked the buttons to the Page

--- src/Pages/Groups/App.js
+++ src/Pages/Groups/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import SliderButton from './SliderButton';
 import AddDevicePage from '../Devices/AddDevicePage';
 import Header from '../../AppHeader/Header';
+import Profile from '../userProfile/ProfilePage';
 import '../../App.css';
 
 function App() {
@@ -105,6 +106,7 @@ function App() {
                 <Route path="/add-device" element={
                     <AddDevicePage addDeviceToGroup={addDeviceToGroup} />
                 } />
+                <Route path="/profile" element={<Profile />} />
             </Routes>
         </Router>
     );
